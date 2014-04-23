0aefa1813fffee56102d0297cdaaafc86b428171
Frank <fv42wid@yahoo.com>
2014-04-23 04:47:02 -0400

    initial commit, fresh rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tablesorter::Application.config.secret_token = '83ba8c71eef97bb95bbc93de0acbd759b3de107e0da8d2c45052bf44484e084d85f200d06e28d60da4f395a678024e0e0903b087c2dd020f54a139e23f2b0224'
