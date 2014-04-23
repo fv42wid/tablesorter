26735a5f3f5bda0681e00b1353f9c7e8e60bd647
Frank <fv42wid@yahoo.com>
2014-04-23 14:26:28 -0400

    paging works

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,9 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+$(document).ready(function() {
+
+    $('#main_data').tablesorter().tablesorterPager({container: $('#pager'), size: 3});
+
+});
